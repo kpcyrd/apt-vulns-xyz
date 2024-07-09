bd1f8425d46e0128f5fd8abbc19e8e3dd7fa1e98
kpcyrd <git@rxv.cc>
2024-07-09 19:39:19 +0200

    Add reprepro integration

--- src/main.rs
+++ src/main.rs
@@ -16,20 +16,40 @@ const GREEN: Style = Style::new().fg_color(Some(Color::Ansi(AnsiColor::Green)));
 
 #[derive(Debug, clap::Parser)]
 pub struct Args {
-    /// The package that should be built
-    name: Option<String>,
-    /// Skip fetching source code
-    #[arg(long)]
-    skip_fetch: bool,
-    /// Skip unpacking the source code
-    #[arg(long)]
-    skip_extract: bool,
-    /// Skip running the build
-    #[arg(long)]
-    skip_build: bool,
     /// Increase logging output (can be used multiple times)
     #[arg(short, long, global = true, action(ArgAction::Count))]
     verbose: u8,
+    #[command(subcommand)]
+    subcommand: Subcommand,
+}
+
+#[derive(Debug, clap::Parser)]
+pub enum Subcommand {
+    /// Build a project into a .deb package
+    Build {
+        /// The package that should be built
+        name: String,
+        /// Skip fetching source code
+        #[arg(long)]
+        skip_fetch: bool,
+        /// Skip unpacking the source code
+        #[arg(long)]
+        skip_extract: bool,
+        /// Skip running the build
+        #[arg(long)]
+        skip_build: bool,
+    },
+    /// List all configured packages
+    List,
+    /// Publish the reprepro repository
+    Publish {
+        /// Do not actually upload, instead do a dry-run
+        #[arg(short = 'n', long)]
+        dry_run: bool,
+        /// The target to upload to
+        #[arg(long, default_value = "apt:/var/www/html/")]
+        target: String,
+    },
 }
 
 #[derive(Debug, serde::Deserialize)]
@@ -141,122 +161,163 @@ fn main() -> Result<()> {
     };
     env_logger::init_from_env(Env::default().default_filter_or(log_level));
 
-    if let Some(name) = args.name {
-        let path = format!("pkgs/{name}/build.toml");
-        let buf = fs::read_to_string(path)?;
-        let mut config = toml::from_str::<Config>(&buf)?;
+    match args.subcommand {
+        Subcommand::Build {
+            name,
+            skip_fetch,
+            skip_extract,
+            skip_build,
+        } => {
+            let path = format!("pkgs/{name}/build.toml");
+            let buf = fs::read_to_string(path)?;
+            let mut config = toml::from_str::<Config>(&buf)?;
 
-        info!("Preparing git checkout");
-        if !args.skip_fetch {
-            prepare_source(&name, &config)?;
-        }
+            info!("Preparing git checkout");
+            if !skip_fetch {
+                prepare_source(&name, &config)?;
+            }
 
-        let build_path = format!("build/{name}");
-        fs::create_dir_all(&build_path)?;
-        let mut build_dir = RwLock::new(fs::File::open("build/")?);
-        info!("Waiting for build handle");
-        let _lock = build_dir.write()?;
-        info!("Got build handle");
+            let build_path = format!("build/{name}");
+            fs::create_dir_all(&build_path)?;
+            let mut build_dir = RwLock::new(fs::File::open("build/")?);
+            info!("Waiting for build handle");
+            let _lock = build_dir.write()?;
+            info!("Got build handle");
 
-        if !args.skip_extract {
-            fs::remove_dir_all(&build_path)?;
-            extract_source(&name, &mut config)?;
+            if !skip_extract {
+                fs::remove_dir_all(&build_path)?;
+                extract_source(&name, &mut config)?;
 
-            for patch in config.meta.patches {
-                info!("Applying patch: {patch:?}");
-                process::Command::new("patch")
+                for patch in config.meta.patches {
+                    info!("Applying patch: {patch:?}");
+                    process::Command::new("patch")
+                        .args([
+                            "--forward",
+                            "--strip=1",
+                            "-i",
+                            &format!("../../pkgs/{name}/{patch}"),
+                        ])
+                        .current_dir(fs::canonicalize(&build_path)?)
+                        .status()?;
+                }
+            }
+
+            if !skip_build {
+                info!("Starting build");
+                debug!("Executing command: {:?}", config.build.cmd);
+                process::Command::new("repro-env")
                     .args([
-                        "--forward",
-                        "--strip=1",
-                        "-i",
-                        &format!("../../pkgs/{name}/{patch}"),
+                        "build",
+                        "--env",
+                        &format!("DEB_VERSION={}{}", config.meta.version, config.meta.suffix),
+                        "-f",
+                        &format!("../../pkgs/{name}/repro-env.lock"),
+                        "--",
+                        "sh",
+                        "-c",
+                        &config.build.cmd,
                     ])
                     .current_dir(fs::canonicalize(&build_path)?)
                     .status()?;
             }
-        }
 
-        if !args.skip_build {
-            info!("Starting build");
-            debug!("Executing command: {:?}", config.build.cmd);
-            process::Command::new("repro-env")
-                .args([
-                    "build",
-                    "--env",
-                    &format!("DEB_VERSION={}{}", config.meta.version, config.meta.suffix),
-                    "-f",
-                    &format!("../../pkgs/{name}/repro-env.lock"),
-                    "--",
-                    "sh",
-                    "-c",
-                    &config.build.cmd,
-                ])
-                .current_dir(fs::canonicalize(&build_path)?)
-                .status()?;
-        }
-
-        let mut checksum_mismatches = false;
-        for rule in config.checksums {
-            info!("Checking expected checksum for {:?}", rule.path);
-            match rule.verify(&build_path)? {
-                Some(Some(calculated)) => {
-                    error!(
-                        "Compiled artifact ({}) does not match expected checksum ({})",
-                        calculated, rule.checksum
-                    );
-                    checksum_mismatches = true;
-                }
-                Some(None) => (),
-                None => {
-                    error!("Missing compiled artifact {:?}", rule.path);
-                    checksum_mismatches = true;
+            let mut checksum_mismatches = false;
+            for rule in config.checksums {
+                info!("Checking expected checksum for {:?}", rule.path);
+                match rule.verify(&build_path)? {
+                    Some(Some(calculated)) => {
+                        error!(
+                            "Compiled artifact ({}) does not match expected checksum ({})",
+                            calculated, rule.checksum
+                        );
+                        checksum_mismatches = true;
+                    }
+                    Some(None) => (),
+                    None => {
+                        error!("Missing compiled artifact {:?}", rule.path);
+                        checksum_mismatches = true;
+                    }
                 }
             }
+
+            if checksum_mismatches {
+                bail!("Some artifact checksums mismatched");
+            }
         }
+        Subcommand::List => {
+            let mut entries = fs::read_dir("pkgs")?
+                .map(|x| x.map_err(anyhow::Error::from))
+                .collect::<Result<Vec<_>, _>>()?;
+            entries.sort_by_key(|de| de.file_name());
 
-        if checksum_mismatches {
-            bail!("Some artifact checksums mismatched");
-        }
-    } else {
-        let mut entries = fs::read_dir("pkgs")?
-            .map(|x| x.map_err(anyhow::Error::from))
-            .collect::<Result<Vec<_>, _>>()?;
-        entries.sort_by_key(|de| de.file_name());
-
-        for entry in entries {
-            let path = entry.path();
-            let Some(name) = path.file_name() else {
-                continue;
-            };
-            let Some(name) = name.to_str() else { continue };
-
-            let path = path.join("build.toml");
-            let buf = fs::read_to_string(&path)?;
-            let config = toml::from_str::<Config>(&buf)?;
-
-            let build_path = format!("build/{name}");
-
-            let mut built = None;
-            for rule in config.checksums {
-                if built == Some(false) {
+            for entry in entries {
+                let path = entry.path();
+                let Some(name) = path.file_name() else {
                     continue;
                 };
+                let Some(name) = name.to_str() else { continue };
 
-                built = match rule.verify(&build_path)? {
-                    Some(Some(_calculated)) => Some(false),
-                    Some(None) => Some(true),
-                    None => Some(false),
-                };
+                let path = path.join("build.toml");
+                let buf = fs::read_to_string(&path)?;
+                let config = toml::from_str::<Config>(&buf)?;
+
+                let build_path = format!("build/{name}");
+
+                let mut built = None;
+                for rule in config.checksums {
+                    if built == Some(false) {
+                        continue;
+                    };
+
+                    built = match rule.verify(&build_path)? {
+                        Some(Some(_calculated)) => Some(false),
+                        Some(None) => Some(true),
+                        None => Some(false),
+                    };
+                }
+
+                println!(
+                    "{GREEN}{:>7}{GREEN:#} {BOLD}{:>18}{BOLD:#} {CYAN}{:>8}{}{CYAN:#}: {}",
+                    if built == Some(true) { "[built]" } else { "" },
+                    name,
+                    config.meta.version,
+                    config.meta.suffix,
+                    config.meta.repo
+                );
             }
+        }
+        Subcommand::Publish { dry_run, target } => {
+            let flags = if dry_run {
+                info!("Dry run mode is enabled");
+                "-avhPicn"
+            } else {
+                "-avhPi"
+            };
 
-            println!(
-                "{GREEN}{:>7}{GREEN:#} {BOLD}{:>18}{BOLD:#} {CYAN}{:>8}{}{CYAN:#}: {}",
-                if built == Some(true) { "[built]" } else { "" },
-                name,
-                config.meta.version,
-                config.meta.suffix,
-                config.meta.repo
-            );
+            let folders = [
+                ("pool/", "pool", false),
+                ("dists/", "dists", true),
+                ("pool/", "pool", true),
+                ("index.html", "", false),
+                ("kpcyrd.pgp", "", false),
+            ];
+
+            for (src, dst, delete) in folders {
+                info!("Syncing {src:?}...");
+                let mut cmd = process::Command::new("rsync");
+                cmd.arg(flags);
+                if delete {
+                    cmd.arg("--delete");
+                }
+                cmd.arg("--");
+                cmd.arg(src);
+                cmd.arg(format!("{target}/{dst}"));
+                let status = cmd.status()?;
+
+                if !status.success() {
+                    bail!("Command (rsync) did not complete successfully");
+                }
+            }
         }
     }
 
